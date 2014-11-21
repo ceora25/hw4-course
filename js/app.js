33b5eb548844cc4a62ccb7186375c5d7fd18463f
ceora25 <Ringo@wangjiayingdeMacBook-Air.local>
2014-11-21 22:25:54 +0800

    modify

--- js/app.js
+++ js/app.js
@@ -66,9 +66,9 @@ var loadPagesInfo = function(pages){
     var $page = $(tmpl).clone();
     FB.api(item.id, function(response){
       // 塞 name, about, like 數到 html 裡。
-      $page.find('.title a').text('response.name').attr('href',response.link);
-      $page.find('.about').text('response.about');
-      $page.find('.likes').text('response.likes');
+      $page.find('.title a').text(response.name).attr('href',response.link);
+      $page.find('.about').text(response.about);
+      $page.find('.likes').text(response.likes);
 
       FB.api(item.id+'/picture?type=large', function(response){
         // 塞資料到 html 中
